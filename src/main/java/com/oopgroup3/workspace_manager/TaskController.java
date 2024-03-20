8309eee2a9a77a938f2a9aaad29be27621766362
JeremyFooFL <162130360+JeremyFooFL@users.noreply.github.com>
2024-03-21 01:19:44 +0800

    Added Task and to-do list

---
+++ src/main/java/com/oopgroup3/workspace_manager/TaskController.java
@@ -0,0 +1,29 @@
+package com.oopgroup3.workspace_manager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.image.ImageView;
+
+public class TaskController {
+
+    @FXML
+    private AnchorPane taskAnchor;
+
+    @FXML
+    private VBox taskBox;
+
+    @FXML
+    private ImageView addTaskButton;
+    @FXML
+    private ScrollPane taskScroll;
+
+    public void toDo(ActionEvent event) throws IDException {
+
+    }
+
+    private void addToDo throws IDException{
+
+    }
+}
---
+++ src/main/java/com/oopgroup3/workspace_manager/ToDoController.java
@@ -0,0 +1,28 @@
+package com.oopgroup3.workspace_manager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Label;
+
+public class ToDoController {
+
+    @FXML
+    private AnchorPane todoAnchor;
+
+    @FXML
+    private Label taskLabel;
+
+    @FXML
+    private Label deadlineLabel;
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private ImageView tickImage;
+
+}
